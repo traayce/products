eb14669e7683d11ca070e52ad56bb33b902a129e
Unknown <justas.rackauskas@voicehit.com>
2019-01-18 04:12:48 +0200

    connectionString info

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 .NET CORE 2.1 web api + react SPA.
 
 To run api project, you will need .NET CORE 2.1 SDK.
+Database connection string can be found in `api/Services/Registry.cs`
 To apply migration to database run `dotnet ef database update` in `api/DataAccess`
 
 For SPA you will need yarn or npm and node.js
--- api/DataAccess/DatabaseContext.cs
+++ api/DataAccess/DatabaseContext.cs
@@ -5,14 +5,9 @@ namespace DataAccess
 {
     public class DatabaseContext : DbContext
     {
-        const string connectionString = "Data Source=localhost\\MSSQLSERVER01;Initial Catalog=TaskDatabase;Integrated Security=True";
         public DatabaseContext() : base() { }
         public DatabaseContext (DbContextOptions<DatabaseContext> options) : base(options){ }
         public DbSet<ProductEntity> Products { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(connectionString);
-        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
